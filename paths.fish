ac6c3926f3e78138a8303d18e79eda53c7c4dd2e
sproott <david.hrabe12@seznam.cz>
2021-10-15 13:51:24 +0200

    🐛 Fix fish script

--- paths.fish
+++ paths.fish
@@ -2,7 +2,12 @@ set dir /usr/share/paths
 
 function paths
     if [ $argv[1] = go ]
-        cd ($dir/paths go $argv[2])
+        set output ($dir/paths go $argv[2])
+        if [ $status -ne 0 ]
+            echo $output
+            return 1
+        end
+        cd $output
     else
         $dir/paths $argv
     end
