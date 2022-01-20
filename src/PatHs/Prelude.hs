c20a8b180348adb6b9235e292b2eb2e68f99a6f0
sproott <david.hrabe12@seznam.cz>
2022-01-21 00:08:02 +0100

    ➖ Remove `protolude`

--- src/PatHs/Prelude.hs
+++ src/PatHs/Prelude.hs
@@ -1,8 +1,10 @@
-module PatHs.Prelude (module Relude, module Protolude.Safe, safeIOMaybe) where
+module PatHs.Prelude (module Relude, safeIOMaybe, maximumMay) where
 
-import Protolude.Safe
 import Relude hiding (Reader)
 import System.IO.Error (catchIOError)
 
 safeIOMaybe :: IO a -> IO (Maybe a)
 safeIOMaybe m = (Just <$> m) `catchIOError` const (pure Nothing)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay = viaNonEmpty last . sort
