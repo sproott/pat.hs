93a6cf05317050b96456c69296d4a45e22093b41
sproott <david.hrabe12@seznam.cz>
2024-12-19 21:44:08 +0100

    chore: migrate from readFile to readFileBS

--- src/PatHs/Effect/FileSystem.hs
+++ src/PatHs/Effect/FileSystem.hs
@@ -3,10 +3,11 @@
 module PatHs.Effect.FileSystem where
 
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T (decodeUtf8')
 import Effectful
 import Effectful.Dispatch.Dynamic
 import PatHs.Prelude hiding (readFile, writeFile)
-import qualified PatHs.Prelude as IO (readFile, writeFile)
+import qualified PatHs.Prelude as IO (readFileBS, writeFile)
 import qualified System.Directory as IO (createDirectoryIfMissing)
 import Effectful.TH
 
@@ -22,5 +23,5 @@ makeEffect ''FileSystem
 runFileSystemIO :: IOE :> es => Eff (FileSystem : es) a -> Eff es a
 runFileSystemIO = interpret $ \_ -> \case
   CreateDirectoryIfMissing parents dir -> liftIO $ IO.createDirectoryIfMissing parents dir
-  ReadFile path -> liftIO $ safeIOMaybe $ T.pack <$> IO.readFile path
+  ReadFile path -> liftIO $ fmap join $ safeIOMaybe $ fmap rightToMaybe $ T.decodeUtf8' <$> IO.readFileBS path
   WriteFile path contents -> liftIO $ IO.writeFile path $ T.unpack contents
